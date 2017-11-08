5f7d8b3c0532e2289e2da926a8925ba6da1cff71
m1ya <o_izy@yahoo.co.jp>
2017-11-08 23:34:09 +0900

    First commit

---
+++ Assets/ThrowBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour {
+
+	public GameObject pokemonBallPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			GameObject ball = Instantiate(pokemonBallPrefab, Camera.main.transform.position, Camera.main.transform.rotation);
+			ball.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward*500);
+		}
+	}
+}
